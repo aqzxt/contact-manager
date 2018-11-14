632bb578dddcf7eb714fdd6f42d720a273600b54
mxhsb <mxcoder@yandex.com>
2018-11-14 01:06:37 -0200

    first component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>Bruce Wayne</h4>
+                <ul>
+                    <li>Email: bruce@wayne.bat</li>
+                    <li>Phone: 666-666-666</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Contact;
